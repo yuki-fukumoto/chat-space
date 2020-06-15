017989dc6afd197c902dc1769d47cac8cbb1b06e
yuki-fukumoto <yufuyufuyufu@gmail.com>
2020-06-15 21:58:59 +0900

    update respons

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message">
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message">
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  console.log("ok")
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');  
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  return false;
+  });
+});
