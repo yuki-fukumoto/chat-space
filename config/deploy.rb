d0a2a83dde8b97589cb7f03f9199ed174476df16
yuki-fukumoto <yufuyufuyufu@gmail.com>
2020-06-18 22:46:06 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '<3.14.1>'
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, '<chat-space>'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:<yuki-fukumoto>/<chat-space>.git'
+set :repo_url,  'git@github.com:yuki-fukumoto/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
